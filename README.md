e8993358710d91d031b315b116ecb9f6feee6ab9
eagleye123 <49323274+eagleye123@users.noreply.github.com>
2023-03-17 12:42:04 -0600

    Final version

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Calculator
-A calculator in c++. For practice
+# Overview
+
+This software Is design to do basic math, and to do a simple game.
+
+I wanted to get my toes wet into c++. This is my first program, and I wanted to figure out the basics.
+
+
+[Software Demo Video](https://youtu.be/eoAC2i4s1vM)
+
+# Development Environment
+
+I used g++ compilier. I also used random and cmath, for random ints and pi.
+
+# Useful Websites
+
+- [cplusplus](https://cplusplus.com/)
+- [W3Schools](https://www.w3schools.com/cpp/cpp_intro.asp)
+
+# Future Work
+
+
+- Check the inputs for possible null, or string values.
+- Add in a GUI
--- calc.cpp
+++ calc.cpp
@@ -1,30 +1,122 @@
 #include <iostream>
+#include <cmath>
+#include <random>
+
 using namespace std;
 
 
+// Adds the number given and the new number.
 int add(int x) {
     int y;
-    cout << "What would you like to add to" << x;
+    cout << "\n What would you like to add to " << x << " : ";
     cin >> y;
     return x + y;
 }
 
+// Subtracts the number given and the new number.
+int sub(int x) {
+    int y;
+    cout << "\n What would you like to Subtact from " << x << " : ";
+    cin >> y;
+    return x - y;
+}
+
+// Divides the number given and the new number.
+int div(int x) {
+    int y;
+    cout << "\n What would you like to Divide " << x << " By?: ";
+    cin >> y;
+    return x / y;
+}
+
+// Multiplies the number given and the new number.
+int mult(int x) {
+    int y;
+    cout << "\n What would you like to multiply " << x << " By?: ";
+    cin >> y;
+    return x * y;
+}
+
+
+// Finds the radius from circumfrance.
+int rad(int c){
+    return c/(2*M_PI);
+}
+
 int main()
 {
+    // Here we define the variables.
     int s = 0;
-    bool i;
+    bool i = true;
+    int total = 0;
+    string usr_inp;
     string x;
-    // int i; 
-    cout << " \n Would you like to keep going: "; // Type a number and press enter
-    cin >> x; // Get user input from the keyboard
-    if (x == "y"){i = true;};
+    cout << "\n Please Enter Your First Number: ";
+    cin >> total;
+
+    // Here is the while loop that keeps going until the user hits anything but y.
     while (i) {
-        int s = add(s);
-        cout << s;
+
+        cout << "\n Pick the operator you would like to use. (+ - / *): ";
+        cin >> usr_inp;
+        // This goes through each one you can add, and shows you the total after.
+        if (usr_inp == "+")
+        {
+            total = add(total);
+            cout << "\n Total: " << total;
+        };
+        if (usr_inp == "-")
+        {
+            total = sub(total);
+            cout << "\n Total: " << total;
+        };
+        if (usr_inp == "/")
+        {
+            total = div(total);
+            cout << "\n Total: " << total;
+        };
+        if (usr_inp == "*")
+        {
+            total = mult(total);
+            cout << "\n Total: " << total;
+        };
 
 
+        cout << "It's radius would be " << rad(total) << " if you entered in the circumfrence";
         cout << " \n Would you like to keep going: "; // Type a number and press enter
         cin >> x; // Get user input from the keyboard
         if (x != "y"){i = false;};
     }
+
+    cout << "\n alright let's play a new game I'm thinking of something between 1 and 100, if you guess it in 10 trys You WIN! otherwise you loose. What's your first guess?";
+    int guess;
+    cin >> guess;
+
+
+    // Here I add take in a random number between 0 and 100
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<> distr(0, 100);
+    int the_number = distr(gen);
+    int counter = 0;
+
+    // Here I create a while loop that breaks whe you guess right or go over the guess limit.
+    while (guess != the_number || counter == 9){
+        if (guess > the_number){
+            cout << "\n Your guess was too Big";
+        }
+        if (guess < the_number){
+            cout << "\n Your guess was too Small";
+        }
+        cout << "\n What's your new number?";
+        cin >> guess;
+    }
+    if (guess == the_number){
+        cout << "\n \n Congrats you Won!!!!";
+    }
+    else{
+        cout << " \n \n You lost better luck next time.";
+    }
+    
+    cout << "\n \n Well this is the end.";
 }
